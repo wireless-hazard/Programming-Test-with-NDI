e5f995e5427cd933fb6a9a5dff80b46d3402d5e0
wireless-hazard <magnocostamaia@gmail.com>
2024-10-29 20:49:58 -0300

    Add method to read and return the current srcs found and store it

--- NDIlib_Find.cpp
+++ NDIlib_Find.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cassert>
 #include <chrono>
+#include <unordered_map>
+#include <fstream>
 #include <Processing.NDI.Lib.h>
 
 #ifdef _WIN32
@@ -16,10 +18,13 @@ namespace NDIlib::find
     struct NDI_t
     {
     public:
+
+        // Since this is the only class we have from the NDI lib, the init and destroy functions are
+        // also been placed in the constructor and destructor
         NDI_t(void)
         {
             // Not required, but "correct" (see the SDK documentation).
-            assert(NDIlib_initialize()); //probably should use exceptions, but I haven`t use them in C++ yet
+            assert(NDIlib_initialize()); // probably should use exceptions, but I haven`t used them in C++ yet
 
             // We are going to create an NDI finder that locates sources on the network.
             pNDI_find = NDIlib_find_create_v2();
@@ -34,38 +39,69 @@ namespace NDIlib::find
             NDIlib_destroy();
         }
 
-        NDIlib_find_instance_t get_find_handler(void) {return pNDI_find;}
+        bool wait_for_sources(std::chrono::milliseconds timeout)
+        {
+            return NDIlib_find_wait_for_sources(pNDI_find, timeout.count());
+        }
+
+        std::unordered_map<uint32_t, std::string> get_current_sources(void)
+        {
+            uint32_t no_sources = 0U;
+            const NDIlib_source_t* p_sources = NDIlib_find_get_current_sources(pNDI_find, &no_sources);
+
+            for (uint32_t i = 0U; i < no_sources; i++)
+            {
+                // Inserts to the map the current found sources, using the value the p_ndi_name
+                current_sources.insert({i, std::string{p_sources[i].p_ndi_name}});
+            }
+
+            return current_sources;
+        }
 
     private:
+        std::unordered_map<uint32_t, std::string> current_sources;
         NDIlib_find_instance_t pNDI_find;
     };
 }
 
 int main(int argc, char* argv[])
 {
-    NDIlib::find::NDI_t source_list;
+    NDIlib::find::NDI_t source_list_obj;
+    std::ofstream list_of_sources_file;
 
     // Run for one minute
     using namespace std::chrono;
-    for (const auto start = high_resolution_clock::now(); high_resolution_clock::now() - start < minutes(1);) {
+    for (const auto start = high_resolution_clock::now(); high_resolution_clock::now() - start < minutes(1);)
+    {
         // Wait up till 5 seconds to check for new sources to be added or removed
-        if (!NDIlib_find_wait_for_sources(source_list.get_find_handler(), 5000 /* milliseconds */)) {
+        if (!source_list_obj.wait_for_sources(5000ms))
+        {
             std::cout << "No change to the sources found.\n";
             continue;
         }
 
         // Get the updated list of sources
-        uint32_t no_sources = 0;
-        const NDIlib_source_t* p_sources = NDIlib_find_get_current_sources(source_list.get_find_handler(), &no_sources);
+        // the map's key is the source number relative of when it got "probed", 
+        // and the map's value is the p_ndi_name of the source
+        std::unordered_map<uint32_t, std::string> source_list_map = source_list_obj.get_current_sources();
 
-        // Display all the sources.
-        std::cout << "Network sources ("<< no_sources <<" found).\n";
-        for (uint32_t i = 0; i < no_sources; i++)
+        // Opens the file that will store the last array of sources found
+        list_of_sources_file.open("list_of_sources.txt");
+
+        // Display all the sources and write them to the file in a CSV-like format
+        std::cout << "Network sources ("<< source_list_map.size() <<" found).\n";
+
+        // Iterates through the map, printing the keys and values and
+        // writing the values to the file
+        for (const auto& [count, source] : source_list_map)
         {
-            std::cout << i + 1 << ". " << p_sources[i].p_ndi_name <<"<n";
+            std::cout << count + 1 << ". " << source <<"\n";
+            list_of_sources_file << source << "; ";
         }
-    }
 
+        list_of_sources_file.close();
+    }
+    
     // Success. We are done
     return 0;
 }
